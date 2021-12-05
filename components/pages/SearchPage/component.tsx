5a0bb07410ecc2cfba612badd475fb7de73d9277
Yerkanat <yerkanat.makhayev@gmail.com>
2021-12-05 15:09:24 +0600

    feat:prettier

--- components/pages/SearchPage/component.tsx
+++ components/pages/SearchPage/component.tsx
@@ -1,139 +1,165 @@
-import React, {FC, useState, useEffect, useCallback, useMemo} from 'react'
-import CustomDropdown from '../../molecules/CustomDropdown/component'
-import axios from 'axios'
-import {HotelCard} from "../../organisms/HotelCard";
+import React, { FC, useState, useEffect, useCallback, useMemo } from "react";
+import CustomDropdown from "../../molecules/CustomDropdown/component";
+import axios from "axios";
+import { HotelCard } from "../../organisms/HotelCard";
 
 interface hotelData {
-    readonly name?: string; // SALAM ALEIKYM 
-    readonly description?: string;
-    readonly price?: string
-    readonly imageUrl?: string;
-    readonly onClick?: () => void;
+  readonly name?: string; // SALAM ALEIKYM
+  readonly description?: string;
+  readonly price?: string;
+  readonly imageUrl?: string;
+  readonly onClick?: () => void;
 }
 
 export const SearchPage: FC = () => {
-    const [hotelName, setHotelName] = useState<string>("")
-    const [country, setCountry] = useState('')
-    const [features, setFeatures] = useState<Array<string>>([])
-    const [cost, setCost] = useState(0)
-    const [srt, setSrt] = useState('Sort By')
-    const services = useMemo(() => ["SPA", "Swimming Pool", "Bowling", "Yacht"], [])
-    const sortBy = useMemo(() => ["Alphabetically Ascending", "Alphabetically Descending", "Cost Ascending", "Cost Descending"], [])
-    
+  const [hotelName, setHotelName] = useState<string>("");
+  const [country, setCountry] = useState("");
+  const [features, setFeatures] = useState<Array<string>>([]);
+  const [cost, setCost] = useState(0);
+  const [srt, setSrt] = useState("Sort By");
+  const services = useMemo(
+    () => ["SPA", "Swimming Pool", "Bowling", "Yacht"],
+    []
+  );
+  const sortBy = useMemo(
+    () => [
+      "Alphabetically Ascending",
+      "Alphabetically Descending",
+      "Cost Ascending",
+      "Cost Descending",
+    ],
+    []
+  );
 
-    const [serverData, setServerData] = useState<hotelData[]>([]);
+  const [serverData, setServerData] = useState<hotelData[]>([]);
 
-    const setSort = useCallback((sortbythis: string) => {
-            setSrt(sortbythis)
-        },
-        [srt],
-    )
+  const setSort = useCallback(
+    (sortbythis: string) => {
+      setSrt(sortbythis);
+    },
+    [srt]
+  );
 
-    const push = useCallback((feat: string) => { 
-        const hasFeature = features.find((n) => n === feat)
-        if (!hasFeature) {
-            setFeatures((prev) =>  [...prev, feat])
-            console.log(features)
-        }
-        
-    }, [features])
+  const push = useCallback(
+    (feat: string) => {
+      const hasFeature = features.find((n) => n === feat);
+      if (!hasFeature) {
+        setFeatures((prev) => [...prev, feat]);
+        console.log(features);
+      }
+    },
+    [features]
+  );
 
-    useEffect( () => {
-        
+  useEffect(() => {
+    let dataFromReq: hotelData[] = [
+      {
+        name: "ASD",
+        description: "ASDD",
+        price: "228$",
+        imageUrl: "https://",
+        onClick: () => {},
+      },
+      {
+        name: "qwe",
+        description: "qwee",
+        price: "2282$",
+        imageUrl: "https://asd",
+        onClick: () => {},
+      },
+    ];
 
-        let dataFromReq: hotelData[] = [{
-            name: 'ASD',
-            description: 'ASDD',
-            price: '228$',
-            imageUrl: 'https://',
-            onClick: () => {}
-        }, 
-        {
-            name: 'qwe',
-            description: 'qwee',
-            price: '2282$',
-            imageUrl: 'https://asd',
-            onClick: () => {}
-        }] 
-        
-        setServerData(dataFromReq);
-    }, [])
+    setServerData(dataFromReq);
+  }, []);
 
-    let search = () => {
-        console.log("search clicked")
-    }
+  let search = () => {
+    console.log("search clicked");
+  };
 
-
-
-    return (
-        <div className = "container">  
-                <div>
-                    
-                    
-                    <div className = "tw-font-bold tw-inline-block">
-                        <div className = "tw-ml-4">
-                            Hotel Name
-                        </div>
-                        <input type = "text" placeholder = "  hotel name" onChange = {(e) => setHotelName(e.target.value)} className = "tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block" style = {{backgroundColor: "#F7F7F7"}}></input>
-                        
-                    </div>
-
-                    <div className = "tw-font-bold tw-inline-block tw-ml-5">
-                        <div className = "tw-ml-4">
-                            Country 
-                        </div>
-                        <input type = "text" placeholder = "  country" onChange = {(e) => setCountry(e.target.value)} className = "tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block tw-w-64" style = {{backgroundColor: "#F7F7F7"}}></input>
-                        
-                    </div>
-                    <div className = "tw-font-bold tw-inline-block tw-ml-5">
-                        <div className = "tw-ml-4">
-                            Features
-                        </div>
-                        <CustomDropdown onDropClick = {push} items = {services} name = "Features"/>
-                    </div>
-                    <div className = "tw-inline-block ">
-                        
-                        {features.map((item, i) => ( <div key = {i} className = "tw-font-bold tw-inline-block tw-ml-4 btn btn-outline-secondary">{item}</div>))}
-                    </div>
-                </div>
-                <div>
-                    <div className = "tw-font-bold tw-inline-block tw-ml">
-                        <div className = "tw-ml-4">
-                            Cost range
-                        </div>
-                        <input type = "number" placeholder = "  cost" onChange = {(e) => setCost(Number(e.target.value))} className = "tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block" style = {{backgroundColor: "#F7F7F7"}}></input>
-                        
-                    </div>
-                    <div className = "tw-font-bold tw-inline-block tw-ml-5">
-                        <div className = "tw-ml-4">
-                            Sort by
-                        </div>
-                        <CustomDropdown onDropClick = {setSort} items = {sortBy} name = {srt}/>
-
-                        
-                    </div>
-                    <div className = "tw-font-bold tw-inline-block tw-ml-5 tw-w-48">
-                        <button onClick = {search} className = "tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block tw-w-64 tw-text-center" style = {{backgroundColor: "#F7F7F7"}}>
-                            Search
-                        </button>
-                        
-                    </div>
-                    <hr className = "tw-mt-5 tw-mb-5" />
-
-                </div>
-                <div>
-                <div  className = "row">
-
-                {serverData[0] && serverData.filter((item) => item.name?.startsWith(hotelName)).map((item, i) => (
-                    <HotelCard key = {i} className = "col-3"  name = {item.name} price = {item.price} description = {item.description} imageUrl = {item.imageUrl} onClick = {item.onClick} />
-                ))}
-                </div>
-
-
-                    
-                </div>
+  return (
+    <div className="container">
+      <div>
+        <div className="tw-font-bold tw-inline-block">
+          <div className="tw-ml-4">Hotel Name</div>
+          <input
+            type="text"
+            placeholder="  hotel name"
+            onChange={(e) => setHotelName(e.target.value)}
+            className="tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block"
+            style={{ backgroundColor: "#F7F7F7" }}
+          ></input>
         </div>
-    )
-}
-export default SearchPage
 
+        <div className="tw-font-bold tw-inline-block tw-ml-5">
+          <div className="tw-ml-4">Country</div>
+          <input
+            type="text"
+            placeholder="  country"
+            onChange={(e) => setCountry(e.target.value)}
+            className="tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block tw-w-64"
+            style={{ backgroundColor: "#F7F7F7" }}
+          ></input>
+        </div>
+        <div className="tw-font-bold tw-inline-block tw-ml-5">
+          <div className="tw-ml-4">Features</div>
+          <CustomDropdown onDropClick={push} items={services} name="Features" />
+        </div>
+        <div className="tw-inline-block ">
+          {features.map((item, i) => (
+            <div
+              key={i}
+              className="tw-font-bold tw-inline-block tw-ml-4 btn btn-outline-secondary"
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <div className="tw-font-bold tw-inline-block tw-ml">
+          <div className="tw-ml-4">Cost range</div>
+          <input
+            type="number"
+            placeholder="  cost"
+            onChange={(e) => setCost(Number(e.target.value))}
+            className="tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block"
+            style={{ backgroundColor: "#F7F7F7" }}
+          ></input>
+        </div>
+        <div className="tw-font-bold tw-inline-block tw-ml-5">
+          <div className="tw-ml-4">Sort by</div>
+          <CustomDropdown onDropClick={setSort} items={sortBy} name={srt} />
+        </div>
+        <div className="tw-font-bold tw-inline-block tw-ml-5 tw-w-48">
+          <button
+            onClick={search}
+            className="tw-border-solid tw-border-2 tw-rounded-2xl tw-inline-block tw-w-64 tw-text-center"
+            style={{ backgroundColor: "#F7F7F7" }}
+          >
+            Search
+          </button>
+        </div>
+        <hr className="tw-mt-5 tw-mb-5" />
+      </div>
+      <div>
+        <div className="row">
+          {serverData[0] &&
+            serverData
+              .filter((item) => item.name?.startsWith(hotelName))
+              .map((item, i) => (
+                <HotelCard
+                  key={i}
+                  className="col-3"
+                  name={item.name}
+                  price={item.price}
+                  description={item.description}
+                  imageUrl={item.imageUrl}
+                  onClick={item.onClick}
+                />
+              ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SearchPage;
